6d8408b6e3cf45e731b7b26856bc2a117adf1531
MSL <manuel.sierra@bluetab.net>
2018-01-12 13:22:12 +0100

    fixed misspelled copy

--- add_deployment_keys.sh
+++ add_deployment_keys.sh
@@ -5,7 +5,7 @@ echo "$PRODUCTION_PEM" | sed 's/\r//g' | sed 's/^ //g' > ~/.ssh/truebg.pem
 chmod 400 ~/.ssh/truebg.pem
 touch ~/.ssh/config
 chmod 600 ~/.ssh/config
-cp -f ~/.ssh/config ~/.ssh/config.bk2
+cp -f ~/.ssh/config ~/.ssh/config.bk
 PRODUCTION_HOST=$(cat .deliver/config | grep PRODUCTION_HOSTS | cut -f2 -d"=" | sed -e 's/"//g')
 echo "Host ${PRODUCTION_HOST}" > ~/.ssh/config
 echo "IdentityFile ~/.ssh/truebg.pem" >>  ~/.ssh/config
