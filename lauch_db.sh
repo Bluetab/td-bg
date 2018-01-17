dfd6aa583b0fe5d1fd2b21d7aa66055caeaec7d0
ICBluetab <inigo.crespo@bluetab.net>
2018-01-17 11:51:36 +0100

    Remove sudo Add start db shell script

--- lauch_db.sh
+++ lauch_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 passwd=${1:-postgres}
-sudo docker run --name truebg-postgres -p 5432:5432 -e POSTGRES_PASSWORD=$passwd -d postgres:10.1
+docker run --name truebg-postgres -p 5432:5432 -e POSTGRES_PASSWORD=$passwd -d postgres:10.1
