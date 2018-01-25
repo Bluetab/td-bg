46b2becd1d5320eb5e6320ce1a9e1be8ef9e863d
ICBluetab <inigo.crespo@bluetab.net>
2018-01-25 16:15:42 +0100

    Refactor user creation

--- lib/trueBG_web/controllers/user_controller.ex
+++ lib/trueBG_web/controllers/user_controller.ex
@@ -8,54 +8,42 @@ defmodule TrueBGWeb.UserController do
   alias TrueBGWeb.ErrorView
   action_fallback TrueBGWeb.FallbackController
 
-  @user_exist :user_exist
-
   def index(conn, _params) do
     users = Accounts.list_users()
     render(conn, "index.json", users: users)
-  end
-
-  defp exist_user(user_params) do
-    user_name = user_params["user_name"]
-    if user_name == nil do
-      {:valid}
-    else
-      case Accounts.exist_user?(user_name) do
-        false -> {:valid}
-        _ -> {:invalid, @user_exist}
-      end
-    end
   end
 
   defp create_user(user_params) do
     Accounts.create_user(user_params)
   end
 
+  defp do_create(conn, user_params) do
+    with  {:ok, %User{} = user} <- create_user(user_params)
+    do
+      conn
+      |> put_status(:created)
+      |> put_resp_header("location", user_path(conn, :show, user))
+      |> render("show.json", user: user)
+      
+    else
+      _error ->
+        conn
+          |> put_status(:unprocessable_entity)
+          |> render(ErrorView, :"422.json")
+    end
+
+  end
+
   def create(conn, %{"user" => user_params}) do
     current_user = Plug.current_resource(conn)
-    if current_user.is_admin do
-      with  {:valid} <- exist_user(user_params),
-            {:ok, %User{} = user} <- create_user(user_params)
-      do
+    case current_user.is_admin do
+      true ->
         conn
-        |> put_status(:created)
-        |> put_resp_header("location", user_path(conn, :show, user))
-        |> render("show.json", user: user)
-
-      else
-        {:invalid, _error_code} ->
-          conn
-            |> put_status(:unprocessable_entity)
-            |> render(ErrorView, :"422.json")
-        _ ->
-          conn
-            |> put_status(:unprocessable_entity)
-            |> render(ErrorView, :"422.json")
-      end
-    else
-      conn
-      |> put_status(:unauthorized)
-      |> render(ErrorView, :"401.json")
+          |> do_create(user_params)
+      _ ->
+        conn
+          |> put_status(:unauthorized)
+          |> render(ErrorView, :"401.json")
     end
   end
 
