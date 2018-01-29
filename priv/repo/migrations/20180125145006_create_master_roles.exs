c91a4ff28d31e1bac4150af01d25d6450f813446
scuriel <sonia.curiel@bluetab.net>
2018-01-29 09:48:12 +0100

    Scenario "Granting roles to domain group" use global roles in migration

--- priv/repo/migrations/20180125145006_create_master_roles.exs
+++ priv/repo/migrations/20180125145006_create_master_roles.exs
@@ -5,7 +5,8 @@ defmodule TrueBG.Repo.Migrations.CreateMasterRoles do
   alias TrueBG.Permissions.Role
 
   def change do
-    Enum.each ["admin", "watch", "create", "publish"], fn role_name ->
+    Enum.each Role.get_roles, fn role_name ->
+      role_name = Atom.to_string role_name
       Permissions.create_role(%{name: role_name})
     end
   end
