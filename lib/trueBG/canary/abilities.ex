145c37636d1055a762bc080f9348292e12764747
ICBluetab <inigo.crespo@bluetab.net>
2018-02-21 17:49:13 +0100

    Add business concept abilities

--- lib/trueBG/canary/abilities.ex
+++ lib/trueBG/canary/abilities.ex
@@ -4,8 +4,8 @@ defmodule TrueBG.Canary.Abilities do
   alias TrueBG.Taxonomies.DataDomain
   alias TrueBG.Taxonomies.DomainGroup
   alias TrueBG.BusinessConcepts.BusinessConcept
-  alias TrueBG.Permissions
   alias TrueBG.Canary.TaxonomyAbilities
+  alias TrueBG.Canary.BusinessConceptAbilities
   alias TrueBG.Permissions.AclEntry
 
   defimpl Canada.Can, for: User do
@@ -19,11 +19,8 @@ defmodule TrueBG.Canary.Abilities do
     # Data domain
 
     # This is the creation of a business concept in a data domain
-    def can?(%User{id: user_id}, :create_business_concept, %DataDomain{id: data_domain_id})  do
-      %{user_id: user_id,
-        action: :create,
-        data_domain_id: data_domain_id}
-      |> can_execute_action?
+    def can?(%User{} = user, :create_business_concept, %DataDomain{} = data_domain)  do
+      BusinessConceptAbilities.can?(user, :create_business_concept, data_domain)
     end
 
     def can?(%User{} = user, :create_data_domain, %DomainGroup{} = domain_group) do
@@ -38,69 +35,22 @@ defmodule TrueBG.Canary.Abilities do
       true
     end
 
-    def can?(%User{id: user_id}, :update, %BusinessConcept{status: status, data_domain_id: data_domain_id}) do
-      %{user_id: user_id, action: :update,
-        current_status: status,
-        required_statuses: [BusinessConcept.status.draft, BusinessConcept.status.published],
-        data_domain_id: data_domain_id}
-      |> can_execute_action?
+    def can?(%User{} = user, :update, %BusinessConcept{} = business_concept) do
+      BusinessConceptAbilities.can?(user, :update, business_concept)
     end
 
-    def can?(%User{id: user_id}, :send_for_approval, %BusinessConcept{status: status, data_domain_id: data_domain_id}) do
-      %{user_id: user_id, action: :send_for_approval,
-        current_status: status,
-        required_statuses: [BusinessConcept.status.draft],
-        data_domain_id: data_domain_id}
-      |> can_execute_action?
+    def can?(%User{} = user, :send_for_approval, %BusinessConcept{} = business_concept) do
+      BusinessConceptAbilities.can?(user, :send_for_approval, business_concept)
     end
 
-    def can?(%User{id: user_id}, :reject, %BusinessConcept{status: status, data_domain_id: data_domain_id}) do
-      %{user_id: user_id, action: :reject,
-        current_status: status,
-        required_statuses: [BusinessConcept.status.pending_approval],
-        data_domain_id: data_domain_id}
-      |> can_execute_action?
+    def can?(%User{} = user, :reject, %BusinessConcept{} = business_concept) do
+      BusinessConceptAbilities.can?(user, :reject, business_concept)
     end
 
-    def can?(%User{id: user_id}, :publish, %BusinessConcept{status: status, data_domain_id: data_domain_id}) do
-      %{user_id: user_id, action: :publish,
-        current_status: status,
-        required_statuses: [BusinessConcept.status.pending_approval],
-        data_domain_id: data_domain_id}
-      |> can_execute_action?
+    def can?(%User{} = user, :publish, %BusinessConcept{} = business_concept) do
+      BusinessConceptAbilities.can?(user, :publish, business_concept)
     end
 
     def can?(%User{}, _action, _domain),  do: false
-
-    defp can_execute_action?(%{user_id: _user_id,
-                               action: _action,
-                               current_status: current_status,
-                               required_statuses: required_statuses,
-                               data_domain_id: _data_domain_id} = params) do
-      (params |> allowed_action?) &&
-      Enum.member?(required_statuses, current_status)
-    end
-
-    defp can_execute_action?(%{user_id: _user_id,
-                               action: _action,
-                               data_domain_id: _data_domain_id} = params) do
-      params |> allowed_action?
-    end
-
-    defp allowed_action?(%{user_id: user_id, action: action,
-                               data_domain_id: data_domain_id}) do
-
-      role_name = %{user_id: user_id, data_domain_id: data_domain_id}
-      |> Permissions.get_role_in_resource
-      |> Map.get(:name)
-      |> String.to_atom
-
-      permissions = BusinessConcept.get_permissions()
-
-      permissions
-      |> Map.get(role_name)
-      |> Enum.member?(action)
-    end
-
   end
 end
---
+++ lib/trueBG/canary/business_concept_abilities.ex
@@ -0,0 +1,79 @@
+defmodule TrueBG.Canary.BusinessConceptAbilities do
+  @moduledoc false
+  alias TrueBG.Accounts.User
+  alias TrueBG.Taxonomies.DataDomain
+  alias TrueBG.BusinessConcepts.BusinessConcept
+  alias TrueBG.Permissions
+
+  def can?(%User{id: user_id}, :create_business_concept, %DataDomain{id: data_domain_id})  do
+    %{user_id: user_id,
+      action: :create,
+      data_domain_id: data_domain_id}
+    |> can_execute_action?
+  end
+
+  def can?(%User{id: user_id}, :update, %BusinessConcept{status: status, data_domain_id: data_domain_id}) do
+    %{user_id: user_id, action: :update,
+      current_status: status,
+      required_statuses: [BusinessConcept.status.draft, BusinessConcept.status.published],
+      data_domain_id: data_domain_id}
+    |> can_execute_action?
+  end
+
+  def can?(%User{id: user_id}, :send_for_approval, %BusinessConcept{status: status, data_domain_id: data_domain_id}) do
+    %{user_id: user_id, action: :send_for_approval,
+      current_status: status,
+      required_statuses: [BusinessConcept.status.draft],
+      data_domain_id: data_domain_id}
+    |> can_execute_action?
+  end
+
+  def can?(%User{id: user_id}, :reject, %BusinessConcept{status: status, data_domain_id: data_domain_id}) do
+    %{user_id: user_id, action: :reject,
+      current_status: status,
+      required_statuses: [BusinessConcept.status.pending_approval],
+      data_domain_id: data_domain_id}
+    |> can_execute_action?
+  end
+
+  def can?(%User{id: user_id}, :publish, %BusinessConcept{status: status, data_domain_id: data_domain_id}) do
+    %{user_id: user_id, action: :publish,
+      current_status: status,
+      required_statuses: [BusinessConcept.status.pending_approval],
+      data_domain_id: data_domain_id}
+    |> can_execute_action?
+  end
+
+  def can?(%User{}, _action, _domain),  do: false
+
+  defp can_execute_action?(%{user_id: _user_id,
+                             action: _action,
+                             current_status: current_status,
+                             required_statuses: required_statuses,
+                             data_domain_id: _data_domain_id} = params) do
+    (params |> allowed_action?) &&
+    Enum.member?(required_statuses, current_status)
+  end
+
+  defp can_execute_action?(%{user_id: _user_id,
+                             action: _action,
+                             data_domain_id: _data_domain_id} = params) do
+    params |> allowed_action?
+  end
+
+  defp allowed_action?(%{user_id: user_id, action: action,
+                             data_domain_id: data_domain_id}) do
+
+    role_name = %{user_id: user_id, data_domain_id: data_domain_id}
+    |> Permissions.get_role_in_resource
+    |> Map.get(:name)
+    |> String.to_atom
+
+    permissions = BusinessConcept.get_permissions()
+
+    permissions
+    |> Map.get(role_name)
+    |> Enum.member?(action)
+  end
+
+end
