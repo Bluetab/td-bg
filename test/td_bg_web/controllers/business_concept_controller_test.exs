12fa252c6c4927fe7d73c09fdbe2beca6a5189e9
ICBluetab <inigo.crespo@bluetab.net>
2018-03-08 14:23:39 +0100

    Add test for business concept status updates

--- test/td_bg_web/controllers/business_concept_controller_test.exs
+++ test/td_bg_web/controllers/business_concept_controller_test.exs
@@ -4,6 +4,7 @@ defmodule TdBgWeb.BusinessConceptControllerTest do
 
   import TdBgWeb.Authentication, only: :functions
   alias Poison, as: JSON
+  alias TdBg.BusinessConcepts.BusinessConcept
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
@@ -111,6 +112,44 @@ defmodule TdBgWeb.BusinessConceptControllerTest do
       validate_resp_schema(conn, schema, "BusinessConceptResponse")
       assert json_response(conn, 422)["errors"] != %{}
     end
+  end
+
+  describe "update business_concept status" do
+    setup [:create_content_schema]
+
+    @transitions  [{BusinessConcept.status.draft, BusinessConcept.status.pending_approval},
+                   {BusinessConcept.status.pending_approval, BusinessConcept.status.published},
+                   {BusinessConcept.status.pending_approval, BusinessConcept.status.rejected},
+                   {BusinessConcept.status.rejected, BusinessConcept.status.pending_approval},
+                   {BusinessConcept.status.published, BusinessConcept.status.deprecated},
+                  ]
+
+    Enum.each(@transitions, fn(transition) ->
+      status_from = elem(transition, 0)
+      status_to = elem(transition, 1)
+
+      @tag authenticated_user: @admin_user_name, status_from: status_from, status_to: status_to
+      test "update business_concept status change from #{status_from} to #{status_to}", %{conn: conn, swagger_schema: schema, status_from: status_from, status_to: status_to} do
+          user = build(:user)
+          business_concept_version = insert(:business_concept_version, status: status_from, last_change_by:  user.id)
+          business_concept =  business_concept_version.business_concept
+          business_concept_id = business_concept.id
+
+          update_attrs = %{
+            status: status_to,
+          }
+
+          conn = patch conn, business_concept_business_concept_path(conn, :update_status, business_concept), business_concept: update_attrs
+          validate_resp_schema(conn, schema, "BusinessConceptResponse")
+          assert %{"id" => ^business_concept_id} = json_response(conn, 200)["data"]
+
+          conn = recycle_and_put_headers(conn)
+          conn = get conn, business_concept_path(conn, :show, business_concept_id)
+          validate_resp_schema(conn, schema, "BusinessConceptResponse")
+
+          assert json_response(conn, 200)["data"]["status"] == status_to
+      end
+    end)
   end
 
   # describe "delete business_concept" do
