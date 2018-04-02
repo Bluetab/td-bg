2c74e93b29404da09564db5d40f60c0d8413486d
scuriel <sonia.curiel@bluetab.net>
2018-04-02 10:39:48 +0200

    TD-366 search api swagger

--- lib/td_bg_web/controllers/search_controller.ex
+++ lib/td_bg_web/controllers/search_controller.ex
@@ -1,28 +1,53 @@
 defmodule TdBgWeb.SearchController do
   use TdBgWeb, :controller
+  use PhoenixSwagger
+
   alias TdBg.Search
   alias TdBg.ESClientApi
+
+  #  alias TdBgWeb.SwaggerDefinitions
 
   def search(%{body_params: query} = conn, %{"search_id" => index_name}) do
     resp = Search.search(index_name, query)
     json conn, %{data: resp}
   end
 
+  swagger_path :create do
+    post "/search"
+    description "Creates ES indexes"
+    produces "application/json"
+    response 201, "Created"
+    response 500, "Client Error"
+  end
   def create(conn, _params) do
     ESClientApi.create_indexes
-    json conn, %{data: %{status: "created"}}
+    send_resp(conn, :created, "")
   end
 
+  swagger_path :delete do
+    delete "/search"
+    description "Deletes ES indexes"
+    produces "application/json"
+    response 204, "Deleted"
+    response 500, "Client Error"
+  end
   def delete(conn, _params) do
     ESClientApi.delete_indexes
-    json conn, %{data: %{status: "deleted"}}
+    send_resp(conn, :no_content, "")
   end
 
+  swagger_path :reindex_all do
+    get "/search/reindex_all"
+    description "Reindex all ES indexes with DB content"
+    produces "application/json"
+    response 200, "OK"
+    response 500, "Client Error"
+  end
   def reindex_all(conn, _params) do
     {:ok, _response} = Search.put_bulk_search(:domain_group)
     {:ok, _response} = Search.put_bulk_search(:data_domain)
     {:ok, _response} = Search.put_bulk_search(:business_concept)
-    json conn, %{data: %{status: :ok}}
+    send_resp(conn, :ok, "")
   end
 
 end
