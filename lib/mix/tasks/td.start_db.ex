f6d4ff23a3b3ba6efff483c738982649fc38676b
ICBluetab <inigo.crespo@bluetab.net>
2018-04-16 09:25:11 +0200

    Fix mix tasks

--- lib/mix/tasks/td.start_db.ex
+++ lib/mix/tasks/td.start_db.ex
@@ -1,14 +1,14 @@
-defmodule Mix.Tasks.Bg.StopDb do
+defmodule Mix.Tasks.Td.StartDb do
   use Mix.Task
 
-  @shortdoc "Stop postgres container"
+  @shortdoc "Start postgres container"
 
   @moduledoc """
-    Stop postgres container. For linux. No sudo.
+    Start postgres container. For linux. No sudo.
   """
 
   def run(_args) do
-    command_to_run = "docker stop TdBg-postgres"
+    command_to_run = "docker start TdBg-postgres"
     command_to_run_list = String.split(command_to_run, " ")
     [command|command_args] = command_to_run_list
     System.cmd(command, command_args)
