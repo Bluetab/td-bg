525aa2461a2e7cb52d43cce14d3ca3346b3b95f0
Tom Crossland <tom.crossland@bluetab.net>
2018-05-16 17:41:35 +0200

    test: business concept version controller show method

--- test/td_bg_web/controllers/business_concept_version_controller_test.exs
+++ test/td_bg_web/controllers/business_concept_version_controller_test.exs
@@ -18,6 +18,19 @@ defmodule TdBgWeb.BusinessConceptVersionControllerTest do
 
   @admin_user_name "app-admin"
 
+  describe "show" do
+    @tag authenticated_user: @admin_user_name
+    test "shows the specified business_concept_version including it's name, description and content", %{conn: conn} do
+      business_concept_version = insert(:business_concept_version, content: %{"foo" => "bar"}, name: "Concept Name", description: "The awesome concept")
+      business_concept_id = business_concept_version.business_concept.id
+      conn = get conn, business_concept_version_path(conn, :show, business_concept_version.id)
+      data = json_response(conn, 200)["data"]
+      assert data["name"] == business_concept_version.name
+      assert data["description"] == business_concept_version.description
+      assert data["content"] == business_concept_version.content
+    end
+  end
+
   describe "index" do
     @tag authenticated_user: @admin_user_name
     test "lists all business_concept_versions", %{conn: conn} do
@@ -35,16 +48,6 @@ defmodule TdBgWeb.BusinessConceptVersionControllerTest do
       [data] = json_response(conn, 200)["data"]["collection"]
       assert data["name"] == business_concept_version.name
     end
-  end
-
-  def create_template(_) do
-    headers = get_header(get_user_token("app-admin"))
-    attrs = %{}
-      |> Map.put("name", "some type")
-      |> Map.put("content", [])
-    body = %{template: attrs} |> JSON.encode!
-    HTTPoison.post!(template_url(@endpoint, :create), body, headers, [])
-    :ok
   end
 
 end
