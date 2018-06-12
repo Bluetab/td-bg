73357058309325bee9742d6f058b2f60a1b96af3
Tom Crossland <tom.crossland@bluetab.net>
2018-06-13 01:14:53 +0200

    feat: business concept search using elasticsearch

--- lib/td_bg/search/mock_search.ex
+++ lib/td_bg/search/mock_search.ex
@@ -1,10 +1,46 @@
 defmodule TdBg.Search.MockSearch do
   @moduledoc false
 
+  alias TdBg.BusinessConcepts
+  alias TdBg.BusinessConcepts.BusinessConceptVersion
+  alias Poison
+
   def put_search(_something) do
   end
 
   def delete_search(_something) do
   end
 
+  def search("business_concept", %{query: %{bool: %{must: %{match_all: %{}}}}}) do
+    BusinessConcepts.list_all_business_concept_versions()
+    |> Enum.map(&BusinessConceptVersion.search_fields(&1))
+    |> Enum.map(&%{_source: &1})
+    |> Poison.encode!()
+    |> Poison.decode!()
+  end
+
+  def search("business_concept", %{
+        query: %{bool: %{must: %{simple_query_string: %{query: query}}}}
+      }) do
+    BusinessConcepts.list_all_business_concept_versions()
+    |> Enum.map(&BusinessConceptVersion.search_fields(&1))
+    |> Enum.filter(&matches(&1, query))
+    |> Enum.map(&%{_source: &1})
+    |> Poison.encode!()
+    |> Poison.decode!()
+  end
+
+  defp matches(string, query) when is_bitstring(string) do
+    String.starts_with?(string, query)
+  end
+
+  defp matches(list, query) when is_list(list) do
+    list |> Enum.any?(&matches(&1, query))
+  end
+
+  defp matches(map, query) when is_map(map) do
+    map |> Map.values() |> matches(query)
+  end
+
+  defp matches(_item, _query), do: false
 end
