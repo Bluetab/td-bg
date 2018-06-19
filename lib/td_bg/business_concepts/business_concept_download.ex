8787c85c54c241377afdc8875ee9872c5b3a89fc
ICBluetab <inigo.crespo@bluetab.net>
2018-06-19 16:10:32 +0200

    feat: Add separator lines in concepts download

--- lib/td_bg/business_concepts/business_concept_download.ex
+++ lib/td_bg/business_concepts/business_concept_download.ex
@@ -13,24 +13,30 @@ defmodule TdBg.BusinessConcept.Download do
     list = Enum.reduce(types, [], fn(type, acc) ->
       concepts = Map.get(concepts_by_type, type)
       template = Map.get(templates_by_type, type)
-      acc ++ concepts_to_csv(template, concepts)
+
+      csv_list = template_concepts_to_csv(template, concepts, !Enum.empty?(acc))
+      acc ++ csv_list
     end)
 
     to_string(list)
   end
 
-  defp concepts_to_csv(template, concepts) do
+  defp template_concepts_to_csv(template, concepts, add_separation) do
     content = template.content
     content_names  = Enum.reduce(content, [], &(&2 ++ [Map.get(&1, "name")]))
     content_labels = Enum.reduce(content, [], &(&2 ++ [Map.get(&1, "label")]))
     content_names_to_types  = Enum.reduce(content, %{}, &Map.put(&2, Map.get(&1, "name"), Map.get(&1, "type")))
 
+    headers = ["type", "name", "domain", "status"] ++ content_labels
     concepts_list = concepts_to_list(content_names, content_names_to_types, concepts)
+    list_to_encode = case add_separation do
+      true ->
+        empty = build_empty_list([], length(headers))
+        [empty, empty, headers] ++ concepts_list
+      false -> [headers|concepts_list]
+    end
 
-    [["type",
-      "name",
-      "domain",
-      "status"] ++ content_labels| concepts_list]
+    list_to_encode
     |> CSV.encode
     |> Enum.to_list
   end
@@ -42,8 +48,12 @@ defmodule TdBg.BusinessConcept.Download do
                 concept["name"],
                 concept["domain"]["name"],
                 concept["status"]]
+
       acc ++ [Enum.reduce(content_fields, values,
               &(&2 ++ [Map.get(content, &1, "")]))]
     end)
   end
+
+  defp build_empty_list(acc, l) when l < 1, do: acc
+  defp build_empty_list(acc, l), do: [""|build_empty_list(acc, l - 1)]
 end
