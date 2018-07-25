a6053760a44bac8611a4f0550f9a9cedea2f4ef7
Luis Sebastián Huerta <luis.sebastian@bluetab.net>
2018-07-25 16:40:22 +0200

    feat: get name of last_update_by

--- lib/td_bg_web/views/business_concept_version_view.ex
+++ lib/td_bg_web/views/business_concept_version_view.ex
@@ -3,6 +3,7 @@ defmodule TdBgWeb.BusinessConceptVersionView do
   use TdBg.Hypermedia, :view
 
   alias TdBgWeb.BusinessConceptVersionView
+  alias TdPerms.UserCache
 
   def render("index.json", %{
         business_concept_versions: business_concept_versions,
@@ -107,7 +108,7 @@ defmodule TdBgWeb.BusinessConceptVersionView do
       related_to: business_concept_version.related_to,
       name: business_concept_version.name,
       description: business_concept_version.description,
-      last_change_by: business_concept_version.last_change_by,
+      last_change_by: UserCache.get_user(business_concept_version.last_change_by),
       last_change_at: business_concept_version.last_change_at,
       domain: Map.take(business_concept_version.business_concept.domain, [:id, :name]),
       status: business_concept_version.status,
