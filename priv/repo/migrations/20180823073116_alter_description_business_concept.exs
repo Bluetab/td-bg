19832b4c41d9d9aacf9e0b385536cb6ab447ffe8
Nacho <ignacio.hidalgo@bluetab.net>
2018-08-23 10:07:09 +0200

    add migration

---
+++ priv/repo/migrations/20180823073116_alter_description_business_concept.exs
@@ -0,0 +1,9 @@
+defmodule TdBg.Repo.Migrations.AlterDescriptionBusinessConcept do
+  use Ecto.Migration
+
+  def change do
+    alter table(:business_concept_versions) do
+      modify :description, :text
+    end
+  end
+end
