90943c624b31792949da64ab5973933765a576eb
Tom Crossland <tom.crossland@bluetab.net>
2018-09-04 18:49:38 +0200

    refactor: avoid redundant db queries when calculating completeness metric

--- lib/td_bg/metrics/business_concepts.ex
+++ lib/td_bg/metrics/business_concepts.ex
@@ -13,14 +13,14 @@ defmodule TdBg.Metrics.BusinessConcepts do
   @fixed_completness_dimensions [:id, :group, :field, :status, :parent_domains]
 
   @metrics_busines_concepts_on_startup Application.get_env(
-                                       :td_bg,
-                                       :metrics_busines_concepts_on_startup
-                                     )
+                                         :td_bg,
+                                         :metrics_busines_concepts_on_startup
+                                       )
 
   @metrics_publication_frequency Application.get_env(
-                                      :td_bg,
-                                      :metrics_publication_frequency
-                                    )
+                                   :td_bg,
+                                   :metrics_publication_frequency
+                                 )
 
   def start_link do
     GenServer.start_link(__MODULE__, %{})
@@ -29,8 +29,10 @@ defmodule TdBg.Metrics.BusinessConcepts do
   def init(state) do
     if @metrics_busines_concepts_on_startup do
       Instrumenter.setup()
-      schedule_work() # Schedule work to be performed at some point
+      # Schedule work to be performed at some point
+      schedule_work()
     end
+
     {:ok, state}
   end
 
@@ -40,10 +42,20 @@ defmodule TdBg.Metrics.BusinessConcepts do
     Enum.each(concepts_count_metrics, &Instrumenter.set_concepts_count(&1))
 
     concept_fields_completness_metrics = get_concept_fields_completness()
-    Logger.info("Number of concept_fields_completness metric #{inspect(length(concept_fields_completness_metrics))}")
-    Enum.each(concept_fields_completness_metrics, &Instrumenter.set_concept_fields_completness(&1))
 
-    schedule_work() # Reschedule once more
+    Logger.info(
+      "Number of concept_fields_completness metric #{
+        inspect(length(concept_fields_completness_metrics))
+      }"
+    )
+
+    Enum.each(
+      concept_fields_completness_metrics,
+      &Instrumenter.set_concept_fields_completness(&1)
+    )
+
+    # Reschedule once more
+    schedule_work()
     {:noreply, state}
   end
 
@@ -52,20 +64,25 @@ defmodule TdBg.Metrics.BusinessConcepts do
   end
 
   def get_template_map do
-    Templates.list_templates
-      |> Enum.map(&({&1.name, get_template_dimensions(&1)}))
-      |> Map.new
+    Templates.list_templates()
+    |> Enum.map(&{&1.name, get_template_dimensions(&1)})
+    |> Map.new()
+  end
+
+  def get_content_map do
+    Templates.list_templates()
+    |> Enum.map(&{&1.name, &1.content})
+    |> Map.new()
   end
 
   defp get_template_dimensions(template) do
     template
-      |> Map.get(:content)
-      |> Enum.map(&get_name_dimension(&1))
-      |> Enum.filter(&!is_nil(&1))
+    |> Map.get(:content)
+    |> Enum.map(&get_name_dimension(&1))
+    |> Enum.filter(&(!is_nil(&1)))
   end
 
   def get_concepts_count do
-
     search = %{
       query: %{bool: %{must: %{match_all: %{}}}},
       size: 10_000
@@ -74,46 +91,56 @@ defmodule TdBg.Metrics.BusinessConcepts do
     templates_by_name = get_template_map()
 
     @search_service.search("business_concept", search)
-      |> Map.get(:results)
-      |> atomize_concept_map()
-      |> Enum.map(&Map.update!(&1, :parent_domains, fn(current) ->
-          current
-          |> Enum.map(fn(domain) -> domain.name end)
-          |> Enum.join(";")
-        end))
-      |> Enum.map(&Map.update!(&1, :q_rule_count, fn(current) ->
-          case current do
-            0 -> false
-            _ -> true
-          end
-        end))
-      |> Enum.map(&Map.put(&1, :has_quality, Map.get(&1, :q_rule_count)))
-      |> Enum.map(&Map.update!(&1, :link_count, fn(current) ->
-          case current do
-            0 -> false
-            _ -> true
-          end
-        end))
-      |> Enum.map(&Map.put(&1, :has_link, Map.get(&1, :link_count)))
-      |> Enum.map(&({&1, Map.get(templates_by_name, &1.type)}))
-      |> Enum.filter(fn({_concept, template}) -> !is_nil(template) end)
-      |> Enum.map(fn({concept, template}) -> include_template_dimensions(concept, template) end)
-      |> Enum.reduce([], fn(elem, acc) -> [Map.put(elem, :count, 1) |acc] end)
-      |> Enum.group_by(& Enum.zip(
-          get_keys(&1, @fixed_concepts_count_dimensions, Map.get(templates_by_name, &1.type)),
-          get_values(&1, @fixed_concepts_count_dimensions, Map.get(templates_by_name, &1.type)))
+    |> Map.get(:results)
+    |> atomize_concept_map()
+    |> Enum.map(
+      &Map.update!(&1, :parent_domains, fn current ->
+        current
+        |> Enum.map(fn domain -> domain.name end)
+        |> Enum.join(";")
+      end)
+    )
+    |> Enum.map(
+      &Map.update!(&1, :q_rule_count, fn current ->
+        case current do
+          0 -> false
+          _ -> true
+        end
+      end)
+    )
+    |> Enum.map(&Map.put(&1, :has_quality, Map.get(&1, :q_rule_count)))
+    |> Enum.map(
+      &Map.update!(&1, :link_count, fn current ->
+        case current do
+          0 -> false
+          _ -> true
+        end
+      end)
+    )
+    |> Enum.map(&Map.put(&1, :has_link, Map.get(&1, :link_count)))
+    |> Enum.map(&{&1, Map.get(templates_by_name, &1.type)})
+    |> Enum.filter(fn {_concept, template} -> !is_nil(template) end)
+    |> Enum.map(fn {concept, template} -> include_template_dimensions(concept, template) end)
+    |> Enum.reduce([], fn elem, acc -> [Map.put(elem, :count, 1) | acc] end)
+    |> Enum.group_by(
+      &Enum.zip(
+        get_keys(&1, @fixed_concepts_count_dimensions, Map.get(templates_by_name, &1.type)),
+        get_values(&1, @fixed_concepts_count_dimensions, Map.get(templates_by_name, &1.type))
       )
-      |> Enum.map(fn {key, value} ->
-          %{dimensions: Enum.into(key, %{}),
-            count: value |> Enum.map(& &1.count) |> Enum.sum(),
-            template_name: List.first(value).type |> normalize_template_name()}
-        end)
+    )
+    |> Enum.map(fn {key, value} ->
+      %{
+        dimensions: Enum.into(key, %{}),
+        count: value |> Enum.map(& &1.count) |> Enum.sum(),
+        template_name: List.first(value).type |> normalize_template_name()
+      }
+    end)
   end
 
   def normalize_template_name(template_name) do
     template_name
-      |> String.replace(~r/[^A-z\s]/u, "")
-      |> String.replace(~r/\s+/, "_")
+    |> String.replace(~r/[^A-z\s]/u, "")
+    |> String.replace(~r/\s+/, "_")
   end
 
   defp include_empty_metrics_dimensions(concept) do
@@ -123,51 +150,108 @@ defmodule TdBg.Metrics.BusinessConcepts do
   defp include_template_dimensions(concept, template_dimensions) do
     Map.put(
       concept,
-      :content, Map.merge(Enum.into(template_dimensions, %{}, fn(dim) -> {dim, ""} end),
-      concept.content)
+      :content,
+      Map.merge(
+        Enum.into(template_dimensions, %{}, fn dim -> {dim, ""} end),
+        concept.content
+      )
     )
   end
 
   def get_concept_fields_completness do
-
     search = %{
       query: %{bool: %{must: %{match_all: %{}}}},
       size: 10_000
     }
 
-    @search_service.search("business_concept", search)
-      |> Map.get(:results)
-      |> atomize_concept_map()
-      |> Enum.map(&Map.update!(&1, :parent_domains, fn(current) ->
-          current
-          |> Enum.map(fn(domain) -> domain.name end)
-          |> Enum.join(";")
-          end))
-      |> Enum.filter(fn(concept) -> !is_nil(Templates.get_template_by_name(concept.type)) end)
-      |> Enum.map(&include_empty_metrics_dimensions(&1))
+    templates_by_name = get_template_map()
+    content_by_name = get_content_map()
 
-      |> Enum.reduce([], fn(concept, acc) ->
-          [Enum.reduce(get_not_required_fields(concept), [], fn(field, acc) ->
+    @search_service.search("business_concept", search)
+    |> Map.get(:results)
+    |> atomize_concept_map()
+    |> Enum.map(
+      &Map.update!(&1, :parent_domains, fn current ->
+        current
+        |> Enum.map(fn domain -> domain.name end)
+        |> Enum.join(";")
+      end)
+    )
+    |> Enum.map(&{&1, Map.get(templates_by_name, &1.type)})
+    |> Enum.filter(fn {_concept, template} -> !is_nil(template) end)
+    |> Enum.map(fn {concept, template} -> include_template_dimensions(concept, template) end)
+    |> Enum.reduce([], fn concept, acc ->
+      [
+        Enum.reduce(
+          get_not_required_fields(concept, Map.get(content_by_name, concept.type)),
+          [],
+          fn field, acc ->
             case Map.get(concept.content, field) do
-              nil -> [%{dimensions: get_map_dimensions(concept, field),
-                        count: 0,
-                        template_name: concept.type |> normalize_template_name()} |acc]
-              "" -> [%{dimensions: get_map_dimensions(concept, field),
-                       count: 0,
-                       template_name: concept.type |> normalize_template_name()} |acc]
-              _ -> [%{dimensions: get_map_dimensions(concept, field),
-                      count: 1,
-                      template_name: concept.type |> normalize_template_name()} |acc]
+              nil ->
+                [
+                  %{
+                    dimensions:
+                      get_map_dimensions(
+                        concept,
+                        field,
+                        Map.get(templates_by_name, concept.type),
+                        Map.get(content_by_name, concept.type)
+                      ),
+                    count: 0,
+                    template_name: concept.type |> normalize_template_name()
+                  }
+                  | acc
+                ]
+
+              "" ->
+                [
+                  %{
+                    dimensions:
+                      get_map_dimensions(
+                        concept,
+                        field,
+                        Map.get(templates_by_name, concept.type),
+                        Map.get(content_by_name, concept.type)
+                      ),
+                    count: 0,
+                    template_name: concept.type |> normalize_template_name()
+                  }
+                  | acc
+                ]
+
+              _ ->
+                [
+                  %{
+                    dimensions:
+                      get_map_dimensions(
+                        concept,
+                        field,
+                        Map.get(templates_by_name, concept.type),
+                        Map.get(content_by_name, concept.type)
+                      ),
+                    count: 1,
+                    template_name: concept.type |> normalize_template_name()
+                  }
+                  | acc
+                ]
             end
-          end) |acc]
-        end) |> List.flatten
+          end
+        )
+        | acc
+      ]
+    end)
+    |> List.flatten()
   end
 
-  defp get_map_dimensions(concept, field) do
-    Enum.into(Enum.zip(
-      get_keys(concept, @fixed_completness_dimensions) ++ [:group, :field],
-      get_values(concept, @fixed_completness_dimensions) ++ get_concept_field_and_group(concept, field)),
-    %{})
+  defp get_map_dimensions(concept, field, template_dimensions, content) do
+    Enum.into(
+      Enum.zip(
+        get_keys(concept, @fixed_completness_dimensions, template_dimensions) ++ [:group, :field],
+        get_values(concept, @fixed_completness_dimensions, template_dimensions) ++
+          get_field_and_group(field, content)
+      ),
+      %{}
+    )
   end
 
   defp get_keys(concept, fixed_dimensions) do
@@ -177,7 +261,7 @@ defmodule TdBg.Metrics.BusinessConcepts do
 
   defp get_keys(concept, fixed_dimensions, template_dimensions) do
     Map.keys(Map.take(concept, fixed_dimensions)) ++
-    Map.keys(Map.take(concept.content, template_dimensions))
+      Map.keys(Map.take(concept.content, template_dimensions))
   end
 
   defp get_values(concept, fixed_dimensions) do
@@ -187,13 +271,18 @@ defmodule TdBg.Metrics.BusinessConcepts do
 
   defp get_values(concept, fixed_dimensions, template_dimensions) do
     Map.values(Map.take(concept, fixed_dimensions)) ++
-    Map.values(Map.take(concept.content, template_dimensions))
+      Map.values(Map.take(concept.content, template_dimensions))
   end
 
-  defp get_concept_field_and_group(concept, field) do
-    group = Templates.get_template_by_name(concept.type).content
-      |> Enum.map(fn (x) -> if field == String.to_atom(x["name"]) do x["group"] end end)
-      |> Enum.filter(fn(elem) -> !is_nil(elem) end)
+  defp get_field_and_group(field, content) do
+    group =
+      content
+      |> Enum.map(fn x ->
+        if field == String.to_atom(x["name"]) do
+          x["group"]
+        end
+      end)
+      |> Enum.filter(fn elem -> !is_nil(elem) end)
 
     case length(group) do
       0 -> ["No group"] ++ [field]
@@ -201,8 +290,8 @@ defmodule TdBg.Metrics.BusinessConcepts do
     end
   end
 
-  defp get_not_required_fields(concept) do
-    Enum.reduce(Map.get(Templates.get_template_by_name(Map.get(concept, :type)), :content), [], fn(field, acc) ->
+  defp get_not_required_fields(concept, content) do
+    Enum.reduce(content, [], fn field, acc ->
       if Map.get(CollectionUtils.atomize_keys(field), :required, false) do
         acc ++ []
       else
@@ -215,31 +304,48 @@ defmodule TdBg.Metrics.BusinessConcepts do
     business_concept_version
     |> Enum.map(&Map.get(&1, "_source"))
     |> Enum.map(&Map.put(&1, "content", CollectionUtils.atomize_keys(Map.get(&1, "content"))))
-    |> Enum.map(&Map.put(&1, "parent_domains", Enum.map(Map.get(&1, "domain_parents"), fn(domain) -> CollectionUtils.atomize_keys(domain) end)))
+    |> Enum.map(
+      &Map.put(
+        &1,
+        "parent_domains",
+        Enum.map(Map.get(&1, "domain_parents"), fn domain ->
+          CollectionUtils.atomize_keys(domain)
+        end)
+      )
+    )
     |> Enum.map(&CollectionUtils.atomize_keys(&1))
   end
 
   def get_dimensions_from_templates do
     Templates.list_templates()
-      |> Enum.map(fn(template) -> %{name: template.name, dimensions: template.content |> Enum.map(fn(content) ->
-          get_name_dimension(content) end)}
-        end)
-      |> Enum.map(&Map.update!(&1, :dimensions, fn(current_dimensions) ->
-          Enum.filter(current_dimensions, fn(d) -> !is_nil(d) end)
-        end))
+    |> Enum.map(fn template ->
+      %{
+        name: template.name,
+        dimensions:
+          template.content
+          |> Enum.map(fn content ->
+            get_name_dimension(content)
+          end)
+      }
+    end)
+    |> Enum.map(
+      &Map.update!(&1, :dimensions, fn current_dimensions ->
+        Enum.filter(current_dimensions, fn d -> !is_nil(d) end)
+      end)
+    )
   end
 
   def get_concept_template_dimensions(concept_type) do
     concept_type
-      |> Templates.get_template_by_name()
-      |> Map.get(:content)
-      |> Enum.map(&get_name_dimension(&1))
-      |> Enum.filter(&!is_nil(&1))
+    |> Templates.get_template_by_name()
+    |> Map.get(:content)
+    |> Enum.map(&get_name_dimension(&1))
+    |> Enum.filter(&(!is_nil(&1)))
   end
 
   defp get_name_dimension(%{"metrics_dimension" => true} = content) do
     String.to_atom(Map.get(content, "name"))
   end
-  defp get_name_dimension(_content), do: nil
 
+  defp get_name_dimension(_content), do: nil
 end
