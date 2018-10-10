700100d05c8f0483ef0a4cc7a9ee06e13f2c34fd
ICBluetab <inigo.crespo@bluetab.net>
2018-10-10 13:11:04 +0200

    feat: Add role view confidential concepts

--- config/config.exs
+++ config/config.exs
@@ -93,7 +93,8 @@ config :td_perms, permissions: [
   :view_rejected_business_concepts,
   :view_deprecated_business_concepts,
   :manage_business_concept_links,
-  :manage_quality_rule
+  :manage_quality_rule,
+  :view_confidential_business_concepts
 ]
 
 # Import environment specific config. This must remain at the bottom
