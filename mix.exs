48e5dfd09e8a7c91f145fd6eae2d2dca74386972
Tom Crossland <tom.crossland@bluetab.net>
2018-10-17 15:11:23 +0200

    feat: obtain app version from environment

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule TdBg.Mixfile do
   def project do
     [
       app: :td_bg,
-      version: "2.6.0",
+      version: case System.get_env("APP_VERSION") do nil -> "2.6.0-local"; v -> v end,
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
