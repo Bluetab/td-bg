31e1bc21b6f5f12e4922bd03957eb9323da9182f
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-11-13 17:28:22 +0100

    feat: New migrations for domains' soft deletion adition

---
+++ priv/repo/migrations/20181113113615_remove_domain_name_unique_constraint.exs
@@ -0,0 +1,10 @@
+defmodule TdBg.Repo.Migrations.RemoveDomainNameUniqueConstraint do
+  @moduledoc """
+  Name field should no longer be an unique index
+  """
+  use Ecto.Migration
+
+  def change do
+    drop unique_index(:domains, [:name], name: :index_domain_by_name)
+  end
+end
---
+++ priv/repo/migrations/20181113115454_add_new_field_deleted_at_in_domains.exs
@@ -0,0 +1,14 @@
+defmodule TdBg.Repo.Migrations.AddNewFieldDeletedAtInDomains do
+  @moduledoc """
+  New field deleted_at is added in order to implement soft deletion
+  """
+  use Ecto.Migration
+
+  def up do
+    alter table(:domains), do: add :deleted_at, :utc_datetime
+  end
+
+  def down do
+    alter table(:domains), do: remove :deleted_at
+  end
+end
