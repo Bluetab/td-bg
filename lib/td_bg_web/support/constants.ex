a5c153a52eaa42b4453b5725a9a0fab2f2732f91
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-11-14 11:57:53 +0100

    chore: New errors format for domains

--- lib/td_bg_web/support/constants.ex
+++ lib/td_bg_web/support/constants.ex
@@ -2,7 +2,8 @@ defmodule TdBg.ErrorConstantsSupport do
   @moduledoc false
   @taxonomy_support_errors %{
     existing_child_domain: %{code: "ETD001", name: "error.existing.domain"},
-    existing_child_business_concept: %{code: "ETD002", name: "error.existing.business.concept"}
+    existing_child_business_concept: %{code: "ETD002", name: "error.existing.business.concept"},
+    existing_domain_with_same_name: %{code: "ETD003", name: "error.existing.domain.name"}
   }
 
   @glossary_support_errors %{
