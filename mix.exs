ff6c73bb648ec98c5a5c42e47ed1cc6f1fc6d429
saul.isern@bluetab.net <saul.isern@bluetab.net>
2018-12-05 15:30:44 +0100

    chore: Update td_perms and td_df_lib version

--- mix.exs
+++ mix.exs
@@ -55,11 +55,11 @@ defmodule TdBg.Mixfile do
       {:csv, "~> 2.0.0"},
       {:nimble_csv, "~> 0.3"},
       {:codepagex, "~> 0.1.4"},
-      {:td_perms, git: "https://github.com/Bluetab/td-perms.git", tag: "2.8.1"},
+      {:td_perms, git: "https://github.com/Bluetab/td-perms.git", tag: "2.8.4"},
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "2.8.3"},
       {:prometheus_ex, "~> 3.0"},
       {:prometheus_plugs, "~> 1.0"},
-      {:grafana, git: "https://github.com/BoringButGreat/grafana.git"},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "2.8.0"}
+      {:grafana, git: "https://github.com/BoringButGreat/grafana.git"}
     ]
   end
 
