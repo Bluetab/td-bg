11acd26b99f851be31739247c5fc05ee2b75c755
netto <euclydes.young@gmail.com>
2019-02-04 18:33:57 +0100

    fix: delete template tables before droping index and tables

--- priv/repo/migrations/20190204144045_drop_templates.exs
+++ priv/repo/migrations/20190204144045_drop_templates.exs
@@ -2,6 +2,7 @@ defmodule TdBg.Repo.Migrations.DropTemplates do
   use Ecto.Migration
 
   def change do
+    execute("delete from templates")
     drop unique_index(:templates, [:name])
     drop table("templates")
   end
