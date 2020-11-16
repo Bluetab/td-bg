f7fd8a87241a0ae58068fe2c09e12948658d7012
Saul Isern <saul.isern@bluetab.net>
2020-11-16 17:05:46 +0100

    chore: [TD-3089] update td-df-lib

--- mix.exs
+++ mix.exs
@@ -74,7 +74,7 @@ defmodule TdBg.Mixfile do
        git: "https://github.com/Bluetab/elasticsearch-elixir.git",
        branch: "feature/bulk-index-action"},
       {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "4.6.0"},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "4.6.0"},
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", branch: "feature/td-3089"},
       {:td_hypermedia, git: "https://github.com/Bluetab/td-hypermedia.git", tag: "4.0.0"},
       {:graph, git: "https://github.com/Bluetab/graph.git", tag: "0.1.1"}
     ]
