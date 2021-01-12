99954cddd207c681d68615826e80ff206e5777ad
elena garcia <elena.garcia@bluetab.net>
2021-01-12 11:20:52 +0100

    fix: [TD-2591] fix test

--- lib/td_bg/business_concepts/business_concepts.ex
+++ lib/td_bg/business_concepts/business_concepts.ex
@@ -399,7 +399,7 @@ defmodule TdBg.BusinessConcepts do
 
   def get_concept_counts(business_concept_id) do
     case ConceptCache.get(business_concept_id, refresh: true) do
-      {:ok, %{rule_count: rule_count, link_count: link_count,concept_count: concept_count}} ->
+      {:ok, %{rule_count: rule_count, link_count: link_count, concept_count: concept_count}} ->
         %{rule_count: rule_count, link_count: link_count, concept_count: concept_count}
 
       _ ->
