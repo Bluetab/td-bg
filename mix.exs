9f8376049f4ea63724dda1f89fffeb87a68d4441
netto <euclydes.young@gmail.com>
2021-03-08 12:56:59 +0100

    chore: update libs version

--- mix.exs
+++ mix.exs
@@ -73,8 +73,8 @@ defmodule TdBg.Mixfile do
       {:elasticsearch,
        git: "https://github.com/Bluetab/elasticsearch-elixir.git",
        branch: "feature/bulk-index-action"},
-      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", branch: "feature/td-3063", override: true},
-      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", branch: "feature/td-3063"},
+      {:td_cache, git: "https://github.com/Bluetab/td-cache.git", tag: "4.15.0"},
+      {:td_df_lib, git: "https://github.com/Bluetab/td-df-lib.git", tag: "4.15.0"},
       {:td_hypermedia, git: "https://github.com/Bluetab/td-hypermedia.git", tag: "4.0.0"},
       {:graph, git: "https://github.com/Bluetab/graph.git", tag: "0.1.1"}
     ]
