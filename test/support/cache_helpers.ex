ede5f468c35ea0cee50159fdda3e0f65907eb7e0
tom.crossland@bluetab.net <tom.crossland@bluetab.net>
2022-03-09 22:17:53 +0000

    TD-4461 bump td_cache

--- test/support/cache_helpers.ex
+++ test/support/cache_helpers.ex
@@ -25,7 +25,7 @@ defmodule CacheHelpers do
 
   def put_domain(%{id: id} = domain) do
     on_exit(fn -> TaxonomyCache.delete_domain(id, clean: true) end)
-    {:ok, _} = TaxonomyCache.put_domain(domain, publish: false)
+    {:ok, _} = TaxonomyCache.put_domain(domain)
   end
 
   def put_concept(%{id: id} = concept) do
