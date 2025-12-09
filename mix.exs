6370e419cc71db041e7a0a4aeda6844e81f69c26
saul.isern@bluetab.net <saul.isern@bluetab.net>
2025-12-09 10:49:14 +0000

    TD-7646 Fix some Concept audit events management

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule TdBg.Mixfile do
       app: :td_bg,
       version:
         case System.get_env("APP_VERSION") do
-          nil -> "7.13.0-local"
+          nil -> "7.13.1-local"
           v -> v
         end,
       elixir: "~> 1.18",
